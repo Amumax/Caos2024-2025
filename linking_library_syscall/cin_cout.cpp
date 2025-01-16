053dfe69a9f7fb66e778ac2af7cd5959dfd0fb54
Lidiya Osipchuk <osipchuk05@list.ru>
2025-01-16 09:35:55 +0000

    1-2 билет

---
+++ linking_library_syscall/cin_cout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    int x;
+    cin >> x;
+    cout << "your number: " << x << endl;  
+    exit(0);
+} 
+
+// ld cin_cout.o /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libc.so.6 
+
+
+/// /usr/lib/gcc/x86_64-linux-gnu/13/crtend.o /usr/lib/gcc/x86_64-linux-gnu/13/crtbegin.o
