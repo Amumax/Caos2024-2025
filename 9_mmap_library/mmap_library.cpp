af926d1171d9e1c74a2761d487550c9135a2b292
AmuMax <maxim.naidyonov@gmail.com>
2025-01-16 14:43:29 +0000

    again

--- 9_mmap_library/mmap_library.cpp
+++ 9_mmap_library/mmap_library.cpp
@@ -8,12 +8,15 @@
 int main(int argc, char* argv[]) {
   const char* file_name = argv[1];
   double argument = strtod(argv[2], NULL);
+
   int fd = open(file_name, O_RDONLY);
   struct stat st = {};
   fstat(fd, &st);
+
   void* addr = mmap(NULL, st.st_size,
   PROT_READ | PROT_EXEC, MAP_PRIVATE, fd, 0);
   double (*func)(double) = (double (*)(double))((char*)addr + 0x40);
+
   close(fd);
   double result = func(argument);
   printf("func(%f) = %f\n", argument, result);
